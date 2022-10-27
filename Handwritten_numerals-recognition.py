d27eecb05efab2a1bb64e98189237b964d43ab90
sharp <sharp@sharp.com>
2022-10-27 23:08:49 +0800

    fix

--- Handwritten_numerals-recognition.py
+++ Handwritten_numerals-recognition.py
@@ -4,15 +4,18 @@ import torch
 import torchvision
 from PIL import Image
 from torch import nn
+from torchvision import  transforms
 import cv2 as cv
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+# 缺点 :  mnist 数据集均为纯黑底白字  对于不是纯黑底白字的数字识别率极低
 
 # conv2d relu maxpool conv2d relu maxpool linear
 # adam crossEntropyLoss
 # nn.Flatten() 默认从dim=1出展开 (60,7,7) 展开为(60,49)
+# nn.Flatten() 可以用img.view(img.size(),-1) 代替,把seq拆成两个
 
-epoch = 8
+epoch = 3
 
 train_data = torchvision.datasets.MNIST("./mnist", True, transform=torchvision.transforms.ToTensor(), download=True)
 test_data = torchvision.datasets.MNIST("./mnist", False, transform=torchvision.transforms.ToTensor(), download=True)
@@ -20,9 +23,12 @@ test_data = torchvision.datasets.MNIST("./mnist", False, transform=torchvision.t
 
 # # image shape =(1,28,28)
 #
-# train_dataLoader=DataLoader(dataset=train_data,batch_size=60,num_workers=0)
-# test_dataLoader=DataLoader(dataset=test_data,batch_size=60,num_workers=0)
+train_dataLoader=DataLoader(dataset=train_data,batch_size=60,num_workers=0)
+test_dataLoader=DataLoader(dataset=test_data,batch_size=60,num_workers=0)
 #
+convert = torchvision.transforms.Compose([transforms.Grayscale(),transforms.Resize((28, 28)),
+                                          transforms.ToTensor()])
+img=convert(Image.open("./num_img_data/9/num_9.png"))
 class cnn(nn.Module):
     def __init__(self):
         super(cnn, self).__init__()
@@ -42,51 +48,44 @@ class cnn(nn.Module):
         return self.seq(img)
 
 
-# nn=cnn()
-# loss_func=torch.nn.CrossEntropyLoss()
-# optim=torch.optim.Adam(nn.parameters(),lr=0.02)
-#
-# start_time = time.time()
-# for i in range(epoch):
-#     nn.train()
-#     cnt=0
-#     print("第{}轮训练开始".format(i + 1))
-#     for imgs,targets in train_dataLoader:
-#         preds=nn(imgs)
-#         loss=loss_func(preds,targets)
-#         optim.zero_grad()
-#         loss.backward()
-#         optim.step()
-#
-#         cnt += 1
-#         if cnt % 1000 == 0:
-#             end_time = time.time()
-#             print("训练时间:{}".format(end_time - start_time))
-#             print("第 {} 次训练loss: {}".format(cnt, loss))
-#     nn.eval()
-#     with torch.no_grad():
-#         if i%2==0:
-#             total_correct=0
-#             for imgs,targets in test_dataLoader:
-#                 preds=nn(imgs)
-#                 loss=loss_func(preds,targets)
-#                 # preds-shape=[60,10]
-#                 # targets-shape=[60]
-#                 total_correct+=(preds.argmax(1)==targets).sum()
-#             print("第{}次训练-测试:accuracy={}".format(i,total_correct/len(test_data)))
-# torch.save(nn,"Handwritten_numerals-recognition_model.pth")
-# print("模型保存成功!")
+nn=torch.load("Handwritten_numerals-recognition_model.pth",map_location=torch.device('cpu'))
+loss_func=torch.nn.CrossEntropyLoss()
+optim=torch.optim.Adam(nn.parameters(),lr=0.02)
+
+start_time = time.time()
+for i in range(epoch):
+    nn.train()
+    for imgs,targets in train_dataLoader:
+        preds=nn(imgs)
+        loss=loss_func(preds,targets)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+    nn.eval()
+    print(nn(img.view(1,1,28,28)))
+    with torch.no_grad():
+        total_correct=0
+        for imgs,targets in test_dataLoader:
+            preds=nn(imgs)
+            loss=loss_func(preds,targets)
+            # preds-shape=[60,10]
+            # targets-shape=[60]
+            total_correct+=(preds.argmax(1)==targets).sum()
+        print("第{}次训练-测试:accuracy={}".format(i,total_correct/len(test_data)))
+torch.save(nn,"Handwritten_numerals-recognition_model.pth")
+print("模型保存成功!")
 
 
 # test model
 # Handwritten_numerals-recognition_model.pth is trained on colab
 # img must be transformed to Tensor
 classifier = torch.load("Handwritten_numerals-recognition_model.pth", map_location=torch.device('cpu'))
-img = Image.open("./num_8.png")
+img = Image.open("num_img_data/1/num_1.png")
 img = img.convert("L")
 convert = torchvision.transforms.Compose([torchvision.transforms.Resize((28, 28)),
                                           torchvision.transforms.ToTensor()])
 img = convert(img)
 img = torch.reshape(img, (1, 1, 28, 28))
 target = classifier(img)
+print(target)
 print("target-num={}".format(target.argmax()))
--- torch_test.py
+++ torch_test.py
@@ -1,19 +1,30 @@
+import matplotlib.pyplot as plt
 import numpy
 import numpy as np
 import cv2 as cv
+import torch.utils.data
 # torch
     # 图像处理 opencv+PIL+transforms
     # 数据统一加载  dataloader
     # 可视化 matplotlib
     # 记录 tensorboard
-# torch.unsqueeze()
+# torch.unsqueeze()维度扩充
+# torch.squeeze()删除大小为1的维度
+# for index,value in enumerate(x):
 
+# transform.ToTensor() 将图像0-255 归一化 0-1
+# transform.Normalize([mean1,mean2,mean3],[std1,std2,std3]) 将三个通道的数据标准化
+# mean ,std 都为0.5 则将数据归一化为-1->1
 
+# data.TensorDataSet(x,y) 生成数据
+
+# torchvision.models 提供训练好的models
 
 import torchvision.datasets
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from torchvision import models
 from PIL import Image
         # # tensorboard
         # # 用[]包住
@@ -33,6 +44,11 @@ from PIL import Image
         # # writer.add_image()  和 writer.add_images()
         # # writer.add_image("test",transforms.ToTensor()(img),2)
         # writer.close()
-# activation function
-import torch.nn.functional as F
-from torch.autograd import Variable
+# dataloader
+# import torch
+# arr=torch.from_numpy(np.asarray([[1,2,3,4],[5,6,4,3],[5,6,3,2],[2,3,4,5]]))
+# transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5],[0.5])])
+
+it=iter([1,2,3,6,2,23])
+print(next(it))
+print(next(it))
